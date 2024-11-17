e5378b6d9f7f934fdc9df60f1f312f8aa78f8dfe
kingjinyong <wlsdydwhswkf@gmail.com>
2024-11-18 07:23:08 +0900

    feat(Book): 책 테이블 생성

---
+++ src/main/java/com/bookmile/backend/domain/book/entity/Book.java
@@ -0,0 +1,29 @@
+package com.bookmile.backend.domain.book.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String bookName;
+
+    private Integer page;
+
+    private String thumbNail;
+
+    private String publisher;
+
+    private String description;
+
+    private String link;
+
+    private Double rating;
+
+
+}
