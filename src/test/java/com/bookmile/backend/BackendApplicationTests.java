7e306ccec286dd3b3e0944b1b221f0582e153693
82everywin <82everywin@gmail.com>
2024-11-17 23:21:02 +0900

    feat: springboot 초기셋팅

---
+++ src/test/java/com/bookmile/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmile.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
