dc2e7719f5bf7a0df43c21c676ea4dcee31ebdf4
kingjinyong <wlsdydwhswkf@gmail.com>
2024-11-18 11:32:40 +0900

    refactor(Review): JoinColumn 어노테이션 적용

--- src/main/java/com/bookmile/backend/domain/review/entity/Review.java
+++ src/main/java/com/bookmile/backend/domain/review/entity/Review.java
@@ -2,11 +2,13 @@ package com.bookmile.backend.domain.review.entity;
 
 import com.bookmile.backend.domain.book.entity.Book;
 import com.bookmile.backend.domain.user.entity.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -17,11 +19,14 @@ public class Review {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
+    @JoinColumn(name = "book_id")
     @ManyToOne(fetch = FetchType.LAZY)
     private Book book;
 
