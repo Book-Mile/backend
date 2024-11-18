89c590508ee5d7efc99f054d5abec5237e97f2a5
kingjinyong <wlsdydwhswkf@gmail.com>
2024-11-18 11:29:42 +0900

    feat(Record): Record 엔티티 생성

--- src/main/java/com/bookmile/backend/domain/userGroup/entity/UserGroup.java
+++ src/main/java/com/bookmile/backend/domain/userGroup/entity/UserGroup.java
@@ -2,7 +2,9 @@ package com.bookmile.backend.domain.userGroup.entity;
 
 import com.bookmile.backend.domain.group.entity.Group;
 import com.bookmile.backend.domain.group.entity.Role;
+import com.bookmile.backend.domain.record.entity.Record;
 import com.bookmile.backend.domain.user.entity.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -12,7 +14,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 
 @Entity
@@ -21,6 +26,7 @@ public class UserGroup {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -30,6 +36,9 @@ public class UserGroup {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id")
     private Group group;
+
+    @OneToMany(mappedBy = "userGroup")
+    private List<Record> record = new ArrayList<>();
 
     @Enumerated(EnumType.STRING)
     private Role role;
