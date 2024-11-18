5781259a7bb06c88192ddc5a82d30fc5b800f0e8
kingjinyong <wlsdydwhswkf@gmail.com>
2024-11-18 11:52:18 +0900

    feat(Record, Image): Image 엔티티 생성

---
+++ src/main/java/com/bookmile/backend/domain/image/entity/Image.java
@@ -0,0 +1,26 @@
+package com.bookmile.backend.domain.image.entity;
+
+import com.bookmile.backend.domain.record.entity.Record;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "record_id")
+    private Record record;
+}
--- src/main/java/com/bookmile/backend/domain/record/entity/Record.java
+++ src/main/java/com/bookmile/backend/domain/record/entity/Record.java
@@ -1,6 +1,8 @@
 package com.bookmile.backend.domain.record.entity;
 
+import com.bookmile.backend.domain.image.entity.Image;
 import com.bookmile.backend.domain.userGroup.entity.UserGroup;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +10,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 import lombok.Getter;
 
 @Entity
@@ -16,11 +20,15 @@ public class Record {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "record_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "usergroup_id")
     private UserGroup userGroup;
+
+    @OneToMany(mappedBy = "record")
+    private List<Image> image;
 
     private String text;
 
