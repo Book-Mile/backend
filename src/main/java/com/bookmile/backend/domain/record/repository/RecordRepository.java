a17d9a3d780535370e5f544636a86792aec8dd36
kingjinyong <wlsdydwhswkf@gmail.com>
2024-11-29 21:11:43 +0900

    feat(RecordRepository): 기록 레포지토리 생성

---
+++ src/main/java/com/bookmile/backend/domain/record/repository/RecordRepository.java
@@ -0,0 +1,10 @@
+package com.bookmile.backend.domain.record.repository;
+
+import com.bookmile.backend.domain.record.entity.Record;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecordRepository extends JpaRepository<Record, Long> {
+    List<Record> findAllByRecordId(Long recordId);
+
+}
