43b22b6a0aeda73cb31e65ec9767dfd5e4b16765
kingjinyong <wlsdydwhswkf@gmail.com>
2024-12-01 11:07:05 +0900

    refacotr(RecordRepository): 필요없는 네이티브 쿼리 제거

--- src/main/java/com/bookmile/backend/domain/record/repository/RecordRepository.java
+++ src/main/java/com/bookmile/backend/domain/record/repository/RecordRepository.java
@@ -3,9 +3,7 @@ package com.bookmile.backend.domain.record.repository;
 import com.bookmile.backend.domain.record.entity.Record;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
-    @Query(value = "SELECT * FROM record WHERE usergroup_id = :userGroupId", nativeQuery = true)
     List<Record> findAllByUserGroupId(Long userGroupId);
 }
