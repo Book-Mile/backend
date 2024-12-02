ffcd306c61bb76757da0dab9832001fbe7752631
82everywin <82everywin@gmail.com>
2024-12-03 02:02:29 +0900

    refactor : 유효성 메세지 변경

--- src/main/java/com/bookmile/backend/domain/user/dto/req/SignUpReqDto.java
+++ src/main/java/com/bookmile/backend/domain/user/dto/req/SignUpReqDto.java
@@ -14,7 +14,7 @@ public class SignUpReqDto {
 
     @NotBlank(message = "이메일은 필수 입력 사항입니다.")
     @Schema(description = "회원의 이메일 주소", example = "user@example.com")
-    @Pattern(regexp="^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$", message="이메일 주소 양식을 확인해주세요.")
+    @Pattern(regexp="^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$", message="이메일 형식이 올바르지 않습니다.")
     private String email;
 
     @NotBlank(message = "비빌번호는 필수 입력 사항입니다.")
