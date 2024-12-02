57029f4777aebdcad35e438dec292bc8bbd3ccb1
kingjinyong <wlsdydwhswkf@gmail.com>
2024-12-02 11:26:06 +0900

    feat(RecordController): 기록 생성&수정 기능 컨트롤러 코드 작성

--- src/main/java/com/bookmile/backend/domain/record/controller/RecordController.java
+++ src/main/java/com/bookmile/backend/domain/record/controller/RecordController.java
@@ -1,11 +1,16 @@
 package com.bookmile.backend.domain.record.controller;
 
 import com.bookmile.backend.domain.record.dto.RecordListResponse;
+import com.bookmile.backend.domain.record.dto.RequestRecord;
 import com.bookmile.backend.domain.record.service.RecordService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +26,18 @@ public class RecordController {
                                                                    @RequestParam Long userId) {
         List<RecordListResponse> records = recordService.viewRecordList(groupId, userId);
         return ResponseEntity.ok(records);
+    }
+
+    @PostMapping
+    public ResponseEntity<Long> createRecord(@RequestParam Long groupId,
+                                             @RequestParam Long userId, @RequestBody RequestRecord requestRecord) {
+        Long recordId = recordService.createRecord(groupId, userId, requestRecord);
+        return ResponseEntity.ok(recordId);
+    }
+
+    @PutMapping("/{recordId}")
+    public ResponseEntity<Long> updateRecord(@PathVariable Long recordId, @RequestBody RequestRecord requestRecord) {
+        Long updateRecord = recordService.updateRecord(recordId, requestRecord);
+        return ResponseEntity.ok(updateRecord);
     }
 }
