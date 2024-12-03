f3c31c3c7ea49e219841bec6984c143ecc5bb3c0
82everywin <82everywin@gmail.com>
2024-12-03 14:31:15 +0900

    feat: 비빈번호 확인 Pattern 제거

--- src/main/java/com/bookmile/backend/domain/user/dto/req/SignUpReqDto.java
+++ src/main/java/com/bookmile/backend/domain/user/dto/req/SignUpReqDto.java
@@ -19,12 +19,12 @@ public class SignUpReqDto {
 
     @NotBlank(message = "비빌번호는 필수 입력 사항입니다.")
     @Schema(description = "회원의 비밀번호", example = "userPassword!")
-    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*]{8,16}$", message = "비밀번호는 8~16자의 영문 대소문자, 숫자, 특수문자로 이루어져야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$", message = "비밀번호는 8~16자의 영문 대소문자, 숫자, 특수문자로 이루어져야 합니다.")
     private String password;
 
-    @NotBlank(message = "비빌번호는 필수 입력 사항입니다.")
+    @NotBlank(message = "비빌번호 확인는 필수 입력 사항입니다.")
     @Schema(description = "회원의 비밀번호 확인", example = "userPassword!")
-    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*]{8,16}$", message = "비밀번호는 8~16자의 영문 대소문자, 숫자, 특수문자로 이루어져야 합니다.")
+    // 확인용이므로 Pattern 이 아니라, password 와 매치하기만 하면 됨.
     private String checkPassword;
 
 
