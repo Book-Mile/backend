29c9ce2c2eae2c52d1ba9585b0eb8255c808c16b
82everywin <82everywin@gmail.com>
2024-12-03 14:31:51 +0900

    refactor: 비밀번호 Pattern 조건 수정

--- src/main/java/com/bookmile/backend/domain/user/dto/req/SignInReqDto.java
+++ src/main/java/com/bookmile/backend/domain/user/dto/req/SignInReqDto.java
@@ -17,7 +17,7 @@ public class SignInReqDto {
 
     @NotBlank(message = "비빌번호는 필수 입력 사항입니다.")
     @Schema(description = "회원의 비밀번호", example = "userPassword!")
-    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*]{8,16}$", message = "비밀번호는 8~16자의 영문 대소문자, 숫자, 특수문자로 이루어져야 합니다.")
+    @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$", message = "비밀번호는 8~16자의 영문 대소문자, 숫자, 특수문자로 이루어져야 합니다.")
     private String password;
 
 }
