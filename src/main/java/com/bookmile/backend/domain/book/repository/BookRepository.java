b6fe999029c630421f61dcdd839a6c6d2e782904
silkair <silkair1234@gmail.com>
2025-01-01 21:09:24 +0900

    feat(repository): 불필요한 외부 api 레포 삭제

--- src/main/java/com/bookmile/backend/domain/book/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bookmile.backend.domain.book.repository;
-
-import com.bookmile.backend.domain.book.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-    boolean existsByTitleAndAuthor(String title, String author);
-}
