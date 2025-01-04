276542b61f90d2d31b975eef4d18415e9102dea3
silkair <silkair1234@gmail.com>
2025-01-05 07:30:14 +0900

    fix : 파일 이름 충돌 수정

--- src/main/java/com/bookmile/backend/domain/book/entity/Book.java
+++ src/main/java/com/bookmile/backend/domain/book/entity/Book.java
@@ -19,7 +19,6 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Book extends BaseEntity {
 
--- src/main/java/com/bookmile/backend/domain/review/service/ReviewBookRepository.java
+++ src/main/java/com/bookmile/backend/domain/review/service/ReviewBookRepository.java
@@ -3,5 +3,5 @@ package com.bookmile.backend.domain.review.service;
 import com.bookmile.backend.domain.book.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface ReviewBookRepository extends JpaRepository<Book, Long> {
 }
