d1faa776e4aa16a8fb22f3cc3ec0e40fcbdd29ea
kingjinyong <wlsdydwhswkf@gmail.com>
2025-01-04 23:04:01 +0900

    feat(ImageController, ImageService, ImageServiceImpl): 이미지 생성 기능 S3 연동 구현
    - 해당 과정에서 ImageContoller의 Service 의존성 주입에서 final 키워드를 제외해서 Lombok이 생성자를 생성하지 않는 오류가 있어서 final 키워드를 적어주었습니다.

--- src/main/java/com/bookmile/backend/domain/image/service/Impl/ImageServiceImpl.java
+++ src/main/java/com/bookmile/backend/domain/image/service/Impl/ImageServiceImpl.java
@@ -3,20 +3,29 @@ package com.bookmile.backend.domain.image.service.Impl;
 import static com.bookmile.backend.global.common.StatusCode.IMAGE_NOT_FOUND;
 import static com.bookmile.backend.global.common.StatusCode.RECORD_NOT_FOUND;
 
-import com.bookmile.backend.domain.image.dto.req.ImageSaveReqDto;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.bookmile.backend.domain.image.entity.Image;
 import com.bookmile.backend.domain.image.repository.ImageRepository;
 import com.bookmile.backend.domain.record.entity.Record;
 import com.bookmile.backend.domain.record.repository.RecordRepository;
 import com.bookmile.backend.global.exception.CustomException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class ImageServiceImpl implements ImageService {
 
+
+    // s3 버킷 클라이언트
+    private final AmazonS3Client s3Client;
     private final ImageRepository imageRepository;
     private final RecordRepository recordRepository;
 
@@ -32,15 +41,46 @@ public class ImageServiceImpl implements ImageService {
     }
 
     @Override
-    public void saveImages(Long recordId, ImageSaveReqDto imageSaveReqDto) {
+    public void saveImages(Long recordId, List<MultipartFile> multipartFiles) throws IOException {
         Record record = recordRepository.findById(recordId)
                 .orElseThrow(() -> new CustomException(RECORD_NOT_FOUND));
+        String bucketName = "bookmile";
 
-        List<Image> images = imageSaveReqDto.getImageUrls().stream()
+        List<String> files = new ArrayList<>();
+        for (MultipartFile file : multipartFiles) {
+            files.add(uploadFileToS3Bucket(bucketName, file));
+        }
+
+        List<Image> images = files.stream()
                 .map(url -> new Image(record, url))
                 .toList();
 
         imageRepository.saveAll(images);
+
+
+    }
+
+
+    // S3에 업로드 하기
+    public String uploadFileToS3Bucket(String bucketName, MultipartFile multiPartFile) throws IOException {
+        File file = convertMultiPartToFile(multiPartFile);
+        String fileName = System.currentTimeMillis() + "_" + multiPartFile.getOriginalFilename();
+
+        s3Client.putObject(new PutObjectRequest(bucketName, fileName, file));
+        file.delete(); // 임시 파일 삭제
+
+        return s3Client.getResourceUrl(bucketName, fileName); // s3에 저장된 파일의 Url 받기
+    }
+
+    // MultiPart 를 파일로 변환 (임시 파일 생성)
+    private File convertMultiPartToFile(MultipartFile multiPartFile) throws IOException {
+        File convFile = new File(multiPartFile.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convFile);
+
+        fos.write(multiPartFile.getBytes());
+        fos.close();
+
+        return convFile;
     }
 
     @Override
