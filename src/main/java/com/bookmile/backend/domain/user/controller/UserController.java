928df4eeb9eeb0e51a87a31ab0a9f5db857b74f1
82everywin <82everywin@gmail.com>
2025-01-06 08:08:25 +0900

    [Docs] Swagger 문서화

--- src/main/java/com/bookmile/backend/domain/user/controller/UserController.java
+++ src/main/java/com/bookmile/backend/domain/user/controller/UserController.java
@@ -7,6 +7,7 @@ import com.bookmile.backend.domain.user.dto.res.UserDetailResDto;
 import com.bookmile.backend.domain.user.dto.res.UserResDto;
 import com.bookmile.backend.domain.user.service.UserService;
 import com.bookmile.backend.global.common.CommonResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,23 +24,27 @@ import static com.bookmile.backend.global.common.StatusCode.*;
 public class UserController {
     private final UserService userService;
 
+    @Operation(summary = "회원가입", description = "소셜로그인이 아닌 '일반 로그인'으로 가입하는 API 입니다.")
     @PostMapping("/sign-up")
     public ResponseEntity<CommonResponse<UserResDto>> signUp(@RequestBody @Valid SignUpReqDto signUpReqDto) {
         return ResponseEntity.status(SIGN_UP.getStatus())
                 .body(CommonResponse.from(SIGN_UP.getMessage(),userService.signUp(signUpReqDto)));
     }
 
+    @Operation(summary = "로그인", description = "'일반 로그인'으로 로그인합니다.")
     @PostMapping("/sign-in")
     public ResponseEntity<CommonResponse<SignInResDto>> signIn(@RequestBody @Valid SignInReqDto signInReqDto) {
         return ResponseEntity.status(SIGN_IN.getStatus())
                 .body(CommonResponse.from(SIGN_IN.getMessage(),userService.signIn(signInReqDto)));
     }
 
+    @Operation(summary = "토큰 재발급", description = "Header 에 refreshToken을 담아 요청을 보내야 합니다.")
     @PostMapping("/reissue")
     public ResponseEntity<CommonResponse<SignInResDto>> reissue(HttpServletRequest request) {
         return ResponseEntity.ok(CommonResponse.from(ISSUED_TOKEN.getMessage(), userService.reIssue(request)));
     }
 
+    @Operation(summary = "회원 정보 조회", description = "access token을 이용하여 회원 정보를 조회합니다.")
     @GetMapping
     public ResponseEntity<CommonResponse<UserDetailResDto>> getUser(@AuthenticationPrincipal UserDetails userDetails) {
         return ResponseEntity.ok(CommonResponse.from(USER_FOUND.getMessage(), userService.getUser(userDetails.getUsername())));
