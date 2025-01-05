1fd538a8d0827d8094f88105a449c7190ff93784
silkair <silkair1234@gmail.com>
2025-01-05 20:59:43 +0900

    fix(dto) : 요청 dto 일부 수정

--- src/main/java/com/bookmile/backend/domain/checkpoint/dto/req/CheckPointRequestDto.java
+++ src/main/java/com/bookmile/backend/domain/checkpoint/dto/req/CheckPointRequestDto.java
@@ -1,6 +1,8 @@
 package com.bookmile.backend.domain.checkpoint.dto.req;
 
 import com.bookmile.backend.domain.checkpoint.entity.GoalType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,11 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CheckPointRequestDto {
 
+    @NotBlank(message = "목표 타입을 선택해주세요.")
+    @Schema(description = "PAGE, CHAPTER, NUMBER 중 택 1", example = "CHAPTER")
     private GoalType goalType;
+
+    @NotBlank(message = "자유 스타일 선택도 가능합니다.")
+    @Schema(description = "FREE", example = "내맘대로 읽을래용")
     private String freeType;
 
-    public CheckPointRequestDto(GoalType goalType, String freeType) {
-        this.goalType = goalType;
-        this.freeType = freeType;
-    }
 }
