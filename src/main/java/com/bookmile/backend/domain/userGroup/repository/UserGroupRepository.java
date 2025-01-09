1d879b954d0e5b4e83ef22cd96b51524d29172c9
silkair <silkair1234@gmail.com>
2025-01-09 14:34:07 +0900

    feat : 그룹 멤버 조회 service 구현

--- src/main/java/com/bookmile/backend/domain/userGroup/repository/UserGroupRepository.java
+++ src/main/java/com/bookmile/backend/domain/userGroup/repository/UserGroupRepository.java
@@ -3,9 +3,14 @@ package com.bookmile.backend.domain.userGroup.repository;
 import com.bookmile.backend.domain.userGroup.entity.UserGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserGroupRepository extends JpaRepository<UserGroup, Long> {
     // 특정 사용자와 그룹 간의 참여 여부 확인
     boolean existsByUserIdAndGroupId(Long userId, Long groupId);
     // 특정 그룹의 현재 참여 인원 수
     int countByGroupId(Long groupId);
+
+    List<UserGroup> findByGroupId(Long groupId);
 }
