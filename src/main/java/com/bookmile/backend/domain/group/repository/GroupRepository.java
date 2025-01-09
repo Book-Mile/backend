addc02d8795ebe835fb77654433014165541a8a8
silkair <silkair1234@gmail.com>
2025-01-10 04:18:43 +0900

    feat : ISBN13 기반 그룹 조회

--- src/main/java/com/bookmile/backend/domain/group/repository/GroupRepository.java
+++ src/main/java/com/bookmile/backend/domain/group/repository/GroupRepository.java
@@ -3,11 +3,17 @@ package com.bookmile.backend.domain.group.repository;
 import com.bookmile.backend.domain.group.entity.Group;
 import com.bookmile.backend.domain.group.entity.GroupStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
     Optional<Group> findByIdAndStatus(Long groupId, GroupStatus status);
+
+    @Query("SELECT g FROM Group g WHERE g.book.isbn13 = :isbn13 AND g.status = :status")
+    List<Group> findByIsbn13AndStatus(@Param("isbn13") String isbn13, @Param("status") GroupStatus status);
 }
