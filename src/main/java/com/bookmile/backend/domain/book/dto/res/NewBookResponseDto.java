a5370bf9c9db401928955940d2f3be410e5f9c81
silkair <silkair1234@gmail.com>
2025-01-10 22:26:43 +0900

    feat : 신간 도서 리스트 응답 dto 구현

---
+++ src/main/java/com/bookmile/backend/domain/book/dto/res/NewBookResponseDto.java
@@ -0,0 +1,15 @@
+package com.bookmile.backend.domain.book.dto.res;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class NewBookResponseDto {
+    private String title;
+    private String author;
+    private String publisher;
+    private String cover;
+    private int customerReviewRank;
+}
