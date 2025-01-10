1045a4297d7628568b3cdea7387b9ba1d26af396
82everywin <82everywin@gmail.com>
2025-01-11 04:11:58 +0900

    feat: responsedto 빌더 추가

--- src/main/java/com/bookmile/backend/domain/group/dto/res/GroupCreateResponseDto.java
+++ src/main/java/com/bookmile/backend/domain/group/dto/res/GroupCreateResponseDto.java
@@ -1,18 +1,44 @@
 package com.bookmile.backend.domain.group.dto.res;
 
+import com.bookmile.backend.domain.group.entity.Group;
+import com.bookmile.backend.domain.template.entity.Template;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class GroupCreateResponseDto {
-    private Long groupId;
-    private String groupName;
-    private int maxMembers;
-    private String goalType;
-    private Long templateId;
-    private String goalContent;
-    private String status;
-    private String groupDescription;
+    private final Long groupId;
+    private final String groupName;
+    private final String groupDescription;
+    private final int maxMembers;
+    private final Long templateId;
+    private final String goalType;
+    private final String goalContent;
+    private final String status;
+
+    @Builder
+    private GroupCreateResponseDto(Long groupId, String groupName, int maxMembers, String goalType,Long templateId, String goalContent, String status, String groupDescription) {
+        this.groupId = groupId;
+        this.groupName = groupName;
+        this.groupDescription = groupDescription;
+        this.maxMembers = maxMembers;
+        this.templateId = templateId;
+        this.goalType = goalType;
+        this.goalContent = goalContent;
+        this.status = status;
+    }
+
+    public static GroupCreateResponseDto toDto(Group group, Template template) {
+        return GroupCreateResponseDto.builder()
+                .groupId(group.getId())
+                .groupName(group.getGroupName())
+                .groupDescription(group.getGroupDescription())
+                .maxMembers(group.getMaxMembers())
+                .templateId(template.getId())
+                .goalType(group.getGoalType())
+                .goalContent(group.getGoalContent())
+                .status(group.getStatus().toString())
+                .build();
+    }
 
 }
