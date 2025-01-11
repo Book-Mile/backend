9d657ece961735f56182daf89a071e1d57cf5cbf
silkair <silkair1234@gmail.com>
2025-01-12 04:44:27 +0900

    feat : 그룹 비공개 전환 요청 dto 구현

---
+++ src/main/java/com/bookmile/backend/domain/group/dto/req/GroupPrivateRequestDto.java
@@ -0,0 +1,10 @@
+package com.bookmile.backend.domain.group.dto.req;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GroupPrivateRequestDto {
+    private Boolean isOpen;
+}
