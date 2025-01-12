9917d6dd9ee998e0f39d7710189c874d513eda52
kingjinyong <wlsdydwhswkf@gmail.com>
2025-01-12 20:10:45 +0900

    refactor(ReviewListResDto): 리뷰 조회시 날짜까지 반환하도록 수정

--- src/main/java/com/bookmile/backend/domain/review/dto/res/ReviewListResDto.java
+++ src/main/java/com/bookmile/backend/domain/review/dto/res/ReviewListResDto.java
@@ -1,6 +1,7 @@
 package com.bookmile.backend.domain.review.dto.res;
 
 import com.bookmile.backend.domain.review.entity.Review;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,13 +12,15 @@ public class ReviewListResDto {
     private String name;
     private Double rating;
     private String text;
+    private LocalDateTime createdAt;
 
     public static ReviewListResDto createReview(Review review) {
         return new ReviewListResDto(
                 review.getId(),
                 review.getUser().getNickname(),
                 review.getRating(),
-                review.getText()
+                review.getText(),
+                review.getCreatedAt()
         );
     }
 }
