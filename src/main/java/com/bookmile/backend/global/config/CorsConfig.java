d5bbab4bed2c5721efa2bb26c1a1ff516ecafde1
82everywin <82everywin@gmail.com>
2025-01-18 23:16:24 +0900

    [Refactor] 배포시 cors에러 수정

--- src/main/java/com/bookmile/backend/global/config/CorsConfig.java
+++ src/main/java/com/bookmile/backend/global/config/CorsConfig.java
@@ -12,8 +12,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://localhost:8080")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("http://localhost:3000","http://localhost:8080", "https://bookmile.site")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
