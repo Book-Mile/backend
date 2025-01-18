3ebe4b085f082a9e06d0f3dfab5997b2d959fc93
82everywin <82everywin@gmail.com>
2025-01-18 22:56:33 +0900

    [Docs] 배포서버주소 연결

--- src/main/java/com/bookmile/backend/BackendApplication.java
+++ src/main/java/com/bookmile/backend/BackendApplication.java
@@ -1,8 +1,13 @@
 package com.bookmile.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@OpenAPIDefinition(servers =
+		{@Server(url = "https://bookmile.site", description = "배포 서버 주소")})
 @SpringBootApplication
 public class BackendApplication {
 
