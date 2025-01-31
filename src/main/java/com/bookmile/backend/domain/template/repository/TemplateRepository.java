df3e99da02f9f0342806db66f16122431943e826
silkair <silkair1234@gmail.com>
2025-02-01 00:17:31 +0900

    fix(repository) : fix repository to use goalType

--- src/main/java/com/bookmile/backend/domain/template/repository/TemplateRepository.java
+++ src/main/java/com/bookmile/backend/domain/template/repository/TemplateRepository.java
@@ -1,16 +1,17 @@
 package com.bookmile.backend.domain.template.repository;
 
+import com.bookmile.backend.domain.template.entity.GoalType;
 import com.bookmile.backend.domain.template.entity.Template;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
 public interface TemplateRepository extends JpaRepository<Template, Long> {
-
-    @Query("SELECT c FROM Template c WHERE c.group.book.id = :bookId AND c.isTemplate = true ORDER BY c.usageCount DESC")
-    List<Template> findTop10PopularTemplatesByBookId(@Param("bookId") Long bookId);
+    @Query("SELECT t FROM Template t WHERE t.group.book.id = :bookId AND t.goalType = :goalType AND t.isTemplate = true ORDER BY t.usageCount DESC")
+    List<Template> findTop10ByBookIdAndGoalType(@Param("bookId") Long bookId, @Param("goalType") GoalType goalType, Pageable pageable);
 }
