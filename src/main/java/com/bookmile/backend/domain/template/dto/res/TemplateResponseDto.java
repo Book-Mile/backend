cff70cfa1a9746680ff58686ba4bfbbea82e0dd7
silkair <silkair1234@gmail.com>
2025-02-02 17:54:12 +0900

    feat : ãadd get masterProfileImage

--- src/main/java/com/bookmile/backend/domain/template/dto/res/TemplateResponseDto.java
+++ src/main/java/com/bookmile/backend/domain/template/dto/res/TemplateResponseDto.java
@@ -3,6 +3,7 @@ package com.bookmile.backend.domain.template.dto.res;
 import com.bookmile.backend.domain.group.entity.Group;
 import com.bookmile.backend.domain.template.entity.GoalType;
 import com.bookmile.backend.domain.template.entity.Template;
+import com.bookmile.backend.domain.user.entity.User;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,19 +15,21 @@ public class TemplateResponseDto {
     private final String goalType;
     private final String goalContent;
     private final int usageCount;
+    private final String masterProfileImage;
 
     @Builder
     public TemplateResponseDto(Long templateId, String groupName, int maxMembers,
-                               GoalType goalType, String goalContent, int usageCount) {
+                               GoalType goalType, String goalContent, int usageCount, String masterProfileImage) {
         this.templateId = templateId;
         this.groupName = groupName;
         this.maxMembers = maxMembers;
         this.goalType = String.valueOf(goalType);
         this.goalContent = goalContent;
         this.usageCount = usageCount;
+        this.masterProfileImage = masterProfileImage;
     }
 
-    public static TemplateResponseDto toDto(Template template, Group group) {
+    public static TemplateResponseDto toDto(Template template, Group group, User masterUser) {
         return TemplateResponseDto.builder()
                 .templateId(template.getId())
                 .groupName(group.getGroupName())
@@ -34,6 +37,7 @@ public class TemplateResponseDto {
                 .goalType(template.getGoalType())
                 .goalContent(template.getGoalContent())
                 .usageCount(template.getUsageCount())
+                .masterProfileImage(masterUser.getImage())
                 .build();
     }
 }
