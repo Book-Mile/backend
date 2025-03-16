f16eb1d238c208eb4ab6e04403ee15ef90618802
82everywin <82everywin@gmail.com>
2025-03-16 11:18:12 +0900

    cors허용

--- src/main/java/com/bookmile/backend/global/config/CorsConfig.java
+++ src/main/java/com/bookmile/backend/global/config/CorsConfig.java
@@ -12,10 +12,18 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "https://bookmile.site","https://bookmile.netlify.app")
+                .allowedOriginPatterns("*")
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:8080", "https://bookmile.site","https://bookmile.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
 
+
+        .addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
